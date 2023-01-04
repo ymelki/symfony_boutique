417e4d108ee2853ef8d6d2e7ce189e7b153b564f
Melki <ymelki@gmail.com>
2023-01-04 10:39:41 +0100

    first commit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/product')]
+#[Route('/Catalog')]
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_product_index', methods: ['GET'])]
